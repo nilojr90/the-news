755219463e9ce2274c861ce5659b7b58f26a06bd
Nilson Ramos de Menezes Júnior <nilojr90@gmail.com>
2021-02-14 01:46:23 -0300

    atribuição da API

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,15 @@
 import React, { ReactElement } from 'react';
 import Home from 'pages/home';
+import nyTimes from 'assets/poweredby_nytimes_200a.png';
 import 'app/App.scss';
 
 function App(): ReactElement {
   return (
     <div className="App">
       <Home />
+      <a href="https://developer.nytimes.com">
+        <img alt="" src={nyTimes} />
+      </a>
     </div>
   );
 }

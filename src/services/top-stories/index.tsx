6945ca20cdad5770d61a45e842ae80c5d76025c0
Nilson Ramos de Menezes Júnior <nilojr90@gmail.com>
2021-02-16 19:37:37 -0300

    feat api-key adicionada ao arquivo .env

--- src/services/top-stories/index.tsx
+++ src/services/top-stories/index.tsx
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 export function getKey(): string {
-  return '2uQnJdaCEfVgxjCqwDgHGc6QAvQLcPzw';
+  return `${process.env.REACT_APP_NWT_API_KEY}`;
 }
 
 const topStoriesApi = axios.create({
-  baseURL:
-    'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=2uQnJdaCEfVgxjCqwDgHGc6QAvQLcPzw',
+  baseURL: `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${getKey()}`,
+  responseType: 'json',
 });
 
 export default topStoriesApi;
